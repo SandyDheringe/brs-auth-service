1e57fd10e3911af6479e4d703bf4959643b68790
sandeep <sandeep.ariseup@gmail.com>
2023-08-26 18:12:45 +0530

    initial commit

---
+++ src/main/java/com/brsauthservice/BrsAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.brsauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrsAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrsAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brsauthservice/BrsAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brsauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrsAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
