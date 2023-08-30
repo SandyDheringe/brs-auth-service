a04dff0bcb95e89852e502c83cd05d6a42b6372c
sandeep <sandeep.ariseup@gmail.com>
2023-08-31 01:12:53 +0530

    docker changes

--- src/main/java/com/brsauthservice/BrsAuthServiceApplication.java
+++ src/main/java/com/brsauthservice/BrsAuthServiceApplication.java
@@ -2,6 +2,7 @@ package com.brsauthservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
@@ -9,7 +10,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class BrsAuthServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BrsAuthServiceApplication.class, args);
+//        SpringApplication.run(BrsAuthServiceApplication.class, args);
+
+        new SpringApplicationBuilder()
+                .profiles("dev")
+                .sources(BrsAuthServiceApplication.class)
+                .run(args);
     }
 
 }
